4e9618b3ccc70b834d69c7b553f20dd0fe66f979
BigPetey72 <lppeters@ucdavis.edu>
2023-04-13 14:39:48 -0700

    sort_cs_weapons
    began sorting by weapon type

--- item_name_loader.py
+++ item_name_loader.py
@@ -1,8 +1,23 @@
-import requests
 import json
+import os
 
-with open('csgo_skins.json', 'r', encoding='utf-8') as csgo_file:
-    csgo_data = json.load(csgo_file)
-    with open('csgo_item_list.json', 'w', encoding='utf-8') as f:
-        for i in csgo_data["items_list"]:
-            print(i, file=f)
+def read_csgo_skins():
+    with open('csgo_skins.json', 'r', encoding='utf-8') as csgo_file:
+        csgo_data = json.load(csgo_file)
+        with open('csgo_item_list.json', 'w', encoding='utf-8') as f:
+            for i in csgo_data["items_list"]:
+                print(i, file=f)
+
+def sort_cs_weapons():
+    path = '/'
+    
+    with open('csgo_skins.json', 'r', encoding='utf-8') as csgo_file:
+        csgo_data = json.load(csgo_file)
+
+    os.chdir('Item_Name_Data')
+    os.chdir('Weapons')
+    files = os.listdir(os.getcwd())
+    print(files)
+
+if __name__ == "__main__":
+    sort_cs_weapons()
